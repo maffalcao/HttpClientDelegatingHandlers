607693f9e3fc6f298c7fa2371d5898b945fc0fc1
marco <maffalcao@gmail.com>
2024-03-18 18:49:23 -0300

    LoggingDelegatingHandler

--- Program.cs
+++ Program.cs
@@ -1,44 +1,21 @@
+using HttpClientDelegatinHadlers.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient<GitHubService>(httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://api.github.com");
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("api/users/{username}", async (
+    string username,
+    GitHubService gitHubService) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var content = await gitHubService.GetByUsernameAsync(username);
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    return Results.Ok(content);
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
